19fc1545e40792839abeee29300913dc9e5dcdee
matheus <matheusmw21@gmail.com>
2024-03-04 07:38:18 -0300

    serving frontend in prod

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
-
+const morgan = require('morgan');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
@@ -8,7 +9,14 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
+
 app.use(planetsRouter);
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(express.json());
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
