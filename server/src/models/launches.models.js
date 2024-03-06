a861afbee25d9f831287581f10734131a32de919
matheus <matheusmw21@gmail.com>
2024-03-06 07:58:12 -0300

    creating launches

--- server/src/models/launches.models.js
+++ server/src/models/launches.models.js
@@ -1,22 +1,38 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
-    date: new Date('December 27, 2030'),
+    launchDate: new Date('December 27, 2030'),
     destination: 'Kepler-442 b',
     customer: ['ZTM','NASA'],
     upcoming: true,
-    success: true,
+    success: true,                  
 };
-
+ 
 launches.set(launch.flightNumber, launch);
 
 function getAllLaunches() {
     return Array.from(launches.values())
 }
 
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(
+        launch.flightNumber,
+        Object.assign(launch, {
+            sucess: true,
+            upcoming: true,
+            customers: ['ZTM','NASA'],
+            flightNumber: latestFlightNumber,
+        })
+    );
+}
+
 module.exports = {
     gettAllLaunches,
-}
+    addNewLaunch,
+};
