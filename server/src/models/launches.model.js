bf9c416b0af14869eb8a05140c966250d2f65290
matheus <matheusmw21@gmail.com>
2024-03-16 11:04:52 -0300

    fix: TypeError

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,49 +3,49 @@ const launches = new Map();
 let latestFlightNumber = 100;
 
 const launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    target: 'Kepler-442 b',
-    customer: ['ZTM','NASA'],
-    upcoming: true,
-    success: true,                  
+  flightNumber: 100,
+  mission: 'Kepler Exploration X',
+  rocket: 'Explorer IS1',
+  launchDate: new Date('December 27, 2030'),
+  target: 'Kepler-442 b',
+  customer: ['ZTM', 'NASA'],
+  upcoming: true,
+  success: true,
 };
- 
+
 launches.set(launch.flightNumber, launch);
 
 function existsLaunchWithId(launchId) {
-    return launches.has(launchId);
+  return launches.has(launchId);
 }
 
 function getAllLaunches() {
-    return Array.from(launches.values())
+  return Array.from(launches.values());
 }
 
 function addNewLaunch(launch) {
-    latestFlightNumber++;
-    launches.set(
-        launch.flightNumber,
-        Object.assign(launch, {
-            success: true,
-            upcoming: true,
-            customers: ['ZTM','NASA'],
-            flightNumber: latestFlightNumber,
-        })
-    );
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ['Zero to Mastery', 'NASA'],
+      flightNumber: latestFlightNumber,
+    })
+  );
 }
 
 function abortLaunchById(launchId) {
-   const aborted = launches.get(launchId);
-   aborted.upcoming = false;
-   aborted.success = false;
-   return aborted;
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
 }
 
 module.exports = {
-    existsLaunchWithId,
-    getAllLaunches,
-    addNewLaunch,
-    abortLaunchById,
+  existsLaunchWithId,
+  getAllLaunches,
+  addNewLaunch,
+  abortLaunchById,
 };
